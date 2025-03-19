7e02c340c12ef76648b4f77067e93e284f47b0f8
Atilla.G <atillagoguslu@hotmail.com>
2025-03-19 19:12:07 +0300

    Completed

--- index.js
+++ index.js
@@ -1,25 +1,36 @@
-document.addEventListener('DOMContentLoaded', function() {
-  const heroButton = document.querySelector('.hero-button');
-  const modalWindow = document.querySelector('.modal-window');
-  const modalButton = document.querySelector('.modal-button');
-  
-  function openModal() {
-    modalWindow.classList.remove('is-hidden');
-    document.body.style.overflow = 'hidden';
-  }
-  
-  function closeModal() {
-    modalWindow.classList.add('is-hidden');
-    document.body.style.overflow = '';
-  }
-  
-  heroButton.addEventListener('click', openModal);
-  modalButton.addEventListener('click', closeModal);
-  
- 
-  const modalForm = document.querySelector('.modal-form');
-  modalForm.addEventListener('submit', function(event) {
-    event.preventDefault();
-    closeModal();
-  });
-}); 
+const heroButton = document.querySelector(".hero-button");
+const modalWindow = document.querySelector(".modal-window");
+const modalButton = document.querySelector(".modal-button");
+const modalForm = document.querySelector(".modal-form");
+
+function openModal() {
+  modalWindow.classList.add("is-open");
+}
+
+function closeModal() {
+  modalWindow.classList.remove("is-open");
+}
+
+heroButton.addEventListener("click", openModal);
+modalButton.addEventListener("click", closeModal);
+
+modalForm.addEventListener("submit", function (event) {
+  event.preventDefault();
+  closeModal();
+});
+
+// Menu Modal
+const menuButton = document.querySelector(".header-menu-svg");
+const menuModal = document.querySelector(".menu-modal");
+const menuCloseButton = document.querySelector(".menu-modal-close-button");
+
+function openMenuModal() {
+  menuModal.classList.add("menu-is-open");
+}
+
+function closeMenuModal() {
+  menuModal.classList.remove("menu-is-open");
+}
+
+menuButton.addEventListener("click", openMenuModal);
+menuCloseButton.addEventListener("click", closeMenuModal);
